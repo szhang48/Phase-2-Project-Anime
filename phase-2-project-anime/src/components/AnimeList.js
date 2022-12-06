bf13e5da801bcadad60102ac8598e5fcc67e6efa
Sandy Zhang <sandyzhang142@gmail.com>
2022-12-06 10:33:06 -0500

    testing

---
+++ phase-2-project-anime/src/components/AnimeList.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+
+
+// function AnimeList({animeList}){
+
+//     return (
+//        <ul className="cards">
+//         {animeList.map((anime)=>(
+//             <AnimeCard anime={anime}/>
+//         ))}
+
+//        </ul>
+//     );
+// }
+
+// export default AnimeList;
---
+++ phase-2-project-anime/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+       Sandy and Sams AnimeFlix
+        <span className="logo" role="img">
+        ༼ つ ◕_◕ ༽つ🍰🍔🍕 "૮₍  ˶•⤙•˶ ₎ა
+        </span>
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
